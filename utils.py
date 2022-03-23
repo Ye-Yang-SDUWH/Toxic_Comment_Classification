b8ba736a26f08a4c57f80a88bea78c7652ae68f2
Zelong <dhcshig@163.com>
2022-03-23 17:47:28 -0400

    fix

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Focal_Loss(nn.Module):
@@ -9,8 +10,8 @@ class Focal_Loss(nn.Module):
         self.gamma = gamma
 
     def forward(self, inputs, targets):
-        criterion = nn.BCELoss()
-        loss = criterion(inputs, targets)
-        pt = torch.exp(-loss)
-        F_loss = self.alpha * (1-pt)**self.gamma * loss
-        return F_loss
+        #criterion = nn.BCELoss()
+        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+        return torch.mean(F_loss)
