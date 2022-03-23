5ae48a17afe586d159ab4c1a202a12f9e7daffe9
Zelong Jiang <zelong@gl-login1.arc-ts.umich.edu>
2022-03-23 17:26:33 -0400

    fix focal loss

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ class Focal_Loss(nn.Module):
 
     def forward(self, inputs, targets):
         # criterion = nn.BCELoss()
-        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+        BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
         pt = torch.exp(-BCE_loss)
         alphas = self.alpha * targets + (1 - self.alpha) * (1 - targets)
         F_loss = alphas * (1-pt)**self.gamma * BCE_loss
